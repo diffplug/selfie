c74196d7f3c949130ffa7fcdee952428ed556514
Ned Twigg <ned.twigg@diffplug.com>
2023-08-28 16:00:46 -0700

    Add a mechanism for tracking writes to disk values.

---
+++ selfie-runner-junit5/src/main/kotlin/com/diffplug/selfie/junit5/WriteTracker.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2023 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.selfie.junit5
+
+import com.diffplug.selfie.RW
+import com.diffplug.selfie.Snapshot
+
+/** Represents the line at which user code called into Selfie. */
+data class CallLocation(val subpath: String, val line: Int) : Comparable<CallLocation> {
+  override fun compareTo(other: CallLocation): Int {
+    val subpathCompare = subpath.compareTo(other.subpath)
+    return if (subpathCompare != 0) subpathCompare else line.compareTo(other.line)
+  }
+}
+/** Represents the callstack above a given CallLocation. */
+internal class CallStack(val location: CallLocation, restOfStack: Any)
+/** Generates a CallLocation and the CallStack behind it. */
+internal fun recordCall(): CallStack = TODO()
+/** The first write at a given spot. */
+internal class FirstWrite<T>(val snapshot: T, val callStack: CallStack)
+
+/** For tracking the writes of disk snapshots literals. */
+internal open class WriteTracker<K : Comparable<K>, V> {
+  val writes = mutableMapOf<K, FirstWrite<V>>()
+  protected fun recordInternal(key: K, snapshot: V, call: CallStack) {
+    val existing = writes.putIfAbsent(key, FirstWrite(snapshot, call))
+    if (existing != null) {
+      if (existing.snapshot != snapshot) {
+        throw org.opentest4j.AssertionFailedError(
+            "Snapshot was set to multiple values:\nfirst time:${existing.callStack}\n\nthis time:${call}",
+            existing.snapshot,
+            snapshot)
+      } else if (RW.isWriteOnce) {
+        throw org.opentest4j.AssertionFailedError(
+            "Snapshot was set to the same value multiple times.", existing.callStack, call)
+      }
+    }
+  }
+}
+
+internal class DiskWriteTracker : WriteTracker<String, Snapshot>() {
+  fun record(key: String, snapshot: Snapshot, call: CallStack) {
+    recordInternal(key, snapshot, call)
+  }
+}
+
+class LiteralValue {
+  // TODO: String, Int, Long, Boolean, etc
+}
+
+internal class InlineWriteTracker : WriteTracker<CallLocation, LiteralValue>() {
+  fun record(call: CallStack, snapshot: LiteralValue) {
+    recordInternal(call.location, snapshot, call)
+  }
+}
