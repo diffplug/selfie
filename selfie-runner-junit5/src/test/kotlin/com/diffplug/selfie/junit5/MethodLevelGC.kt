7cd05b2c6adf26fcb085eb9f49c945e35130650d
Ned Twigg <ned.twigg@diffplug.com>
2023-08-28 00:06:29 -0700

    Add a test for MethodLevelGC.

---
+++ selfie-runner-junit5/src/test/kotlin/com/diffplug/selfie/junit5/MethodLevelGC.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2023 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.selfie.junit5
+
+import kotlin.test.Test
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.TestMethodOrder
+import org.junitpioneer.jupiter.DisableIfTestFails
+
+/** Write-only test which asserts adding and removing snapshots results in same-class GC. */
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@DisableIfTestFails
+class MethodLevelGC : Harness("undertest-junit5") {
+  @Test @Order(1)
+  fun noSelfiesNoFile() {
+    ut_snapshot().deleteIfExists()
+    ut_snapshot().assertDoesNotExist()
+    ut_mirror().linesFrom("UT_AddAndRemoveSnapshots").toLast("}").shrinkByOne().commentOut()
+  }
+
+  @Test @Order(2)
+  fun firstSelfieCreatesFile() {
+    ut_mirror().linesFrom("fun one").toFirst("}").uncomment()
+    gradleWriteSS()
+    ut_snapshot()
+        .assertContent(
+            """
+      ╔═ one ═╗
+      1
+      ╔═ [end of file] ═╗
+      
+    """
+                .trimIndent())
+  }
+
+  @Test @Order(3)
+  fun secondSelfieAppendsFile() {
+    ut_mirror().linesFrom("fun two").toFirst("}").uncomment()
+    gradleWriteSS()
+    ut_snapshot()
+        .assertContent(
+            """
+      ╔═ one ═╗
+      1
+      ╔═ two ═╗
+      2
+      ╔═ [end of file] ═╗
+      
+    """
+                .trimIndent())
+  }
+
+  @Test @Order(4)
+  fun removingSelfieShrinksFile() {
+    ut_mirror().linesFrom("fun one").toFirst("}").commentOut()
+    gradleWriteSS()
+    ut_snapshot()
+        .assertContent(
+            """
+      ╔═ two ═╗
+      2
+      ╔═ [end of file] ═╗
+      
+    """
+                .trimIndent())
+  }
+
+  @Test @Order(5)
+  fun removingAllSelfiesDeletesFile() {
+    ut_mirror().linesFrom("fun two").toFirst("}").commentOut()
+    gradleWriteSS()
+    ut_snapshot().assertDoesNotExist()
+  }
+}
---
+++ undertest-junit5/src/test/kotlin/com/diffplug/selfie/junit5/UT_MethodLevelGC.kt
@@ -0,0 +1,12 @@
+package com.diffplug.selfie.junit5
+// spotless:off
+// spotless:on
+
+class UT_MethodLevelGC {
+//  @Test fun one() {
+//    expectSelfie("1").toMatchDisk()
+//  }
+//  @Test fun two() {
+//    expectSelfie("2").toMatchDisk()
+//  }
+}
