473a92b765b81ee42b4988bcbc0dfd5912422c63
toddriley <toddriley@macbook-pro>
2023-09-29 13:09:40 -0400

    Extract Button to its own component and add wide variant.

---
+++ docs/src/components/Button.tsx
@@ -0,0 +1,58 @@
+import clsx from "clsx";
+import { CSSProperties } from "react";
+
+type ButtonProps = {
+  text: string;
+  isDepressed?: boolean;
+  isWide?: boolean;
+};
+
+export function Button({
+  text,
+  isDepressed = false,
+  isWide = false,
+}: ButtonProps) {
+  // Can't apply TW classes with JS. Custom stuff has to use the old fashioned approach.
+  let style: CSSProperties = {};
+  if (isDepressed) {
+    style = {
+      color: "#FFF",
+      backgroundColor: "#63B9E3",
+    };
+  } else {
+    style = {
+      //   boxShadow: "2px 2px 1px #000", // mobile
+      //   boxShadow: "4px 4px 2px #000", // tablet
+      boxShadow: "4px 4px 2px #000", // desktop
+      color: "#000",
+    };
+  }
+  return (
+    <div
+      className={clsx([
+        "flex",
+        isWide ? "w-[154px]" : "w-[44px]",
+        "justify-center",
+        "rounded-md",
+        "border",
+        "border-2",
+        "border-black",
+        "hover:!text-white",
+        "hover:bg-blue",
+        "cursor-pointer",
+        isWide ? "text-[22px]" : "text-[14px]",
+        isWide ? "tablet:w-[154px]" : "tablet:w-[73px]",
+        "tablet:text-[22px]",
+        "tablet:border-[3px]",
+        "tablet:rounded-lg",
+        isWide ? "desktop:w-[232px]" : "desktop:w-[110px]",
+        "desktop:text-[34px]",
+        "desktop:border-[4px]",
+        "desktop:rounded-xl",
+      ])}
+      style={style}
+    >
+      {text}
+    </div>
+  );
+}
--- docs/src/components/ButtonList.tsx
+++ docs/src/components/ButtonList.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { CSSProperties } from "react";
+import { Button } from "./Button";
 
 export function ButtonList() {
   return (
@@ -19,57 +19,6 @@ export function ButtonList() {
       <Button text="js" />
       <Button text="go" />
       <Button text="..." />
-    </div>
-  );
-}
-
-type ButtonProps = {
-  text: string;
-  isDepressed?: boolean;
-};
-
-function Button({ text, isDepressed = false }: ButtonProps) {
-  // Can't apply TW classes with JS. Custom stuff has to use the old fashioned approach.
-  let style: CSSProperties;
-  if (isDepressed) {
-    style = {
-      color: "#FFF",
-      backgroundColor: "#63B9E3",
-    };
-  } else {
-    style = {
-      //   boxShadow: "2px 2px 1px #000", // mobile
-      //   boxShadow: "4px 4px 2px #000", // tablet
-      boxShadow: "4px 4px 2px #000", // desktop
-      color: "#000",
-    };
-  }
-  return (
-    <div
-      className={clsx([
-        "flex",
-        "w-[44px]",
-        "justify-center",
-        "rounded-md",
-        "border",
-        "border-2",
-        "border-black",
-        "hover:!text-white",
-        "hover:bg-blue",
-        "cursor-pointer",
-        "text-[14px]",
-        "tablet:w-[73px]",
-        "tablet:text-[22px]",
-        "tablet:border-[3px]",
-        "tablet:rounded-lg",
-        "desktop:w-[110px]",
-        "desktop:text-[34px]",
-        "desktop:border-[4px]",
-        "desktop:rounded-xl",
-      ])}
-      style={style}
-    >
-      {text}
     </div>
   );
 }
