5d87c1efe8b1448a02ba452cad6b98028669c32e
toddriley <toddriley@todds-mbp-2>
2023-10-05 10:53:33 -0400

    WIP - nav animation.

--- docs/src/components/Navigation/LanguageSelect.tsx
+++ docs/src/components/Navigation/LanguageSelect.tsx
@@ -1,84 +1,44 @@
 import { LanguageSlug, languageSlugsToLabels } from "@/lib/languageFromPath";
 import { Listbox } from "@headlessui/react";
 import clsx from "clsx";
+import { Dispatch, SetStateAction } from "react";
 import { Button } from "../Button";
 import { CaretBottom } from "../Icons/CaretBottom";
 
 type LanguageSelectProps = {
   selectedLanguage: LanguageSlug;
-  handleChange(value: LanguageSlug): void;
+  setSelectIsOpen: Dispatch<SetStateAction<boolean>>;
+  isOpen: boolean;
 };
 
 export function LanguageSelect({
   selectedLanguage,
-  handleChange,
+  setSelectIsOpen,
+  isOpen,
 }: LanguageSelectProps) {
   return (
     <div className={clsx(["relative", "hidden", "wide-phone:block"])}>
-      <Listbox value={selectedLanguage} onChange={handleChange}>
-        <Listbox.Button>
-          {({ open }) => {
-            return (
-              <Button
-                className={clsx([
-                  "text-white",
-                  "bg-blue",
-                  "w-[110px]",
-                  "rounded-[4px]",
-                ])}
-              >
-                {languageSlugsToLabels[selectedLanguage]}
-                <CaretBottom
-                  className={clsx([
-                    "h-5",
-                    "w-5",
-                    open && "rotate-180",
-                    open && "tablet:rotate-[270deg]",
-                  ])}
-                  fill="#FFFFFF"
-                />
-              </Button>
-            );
-          }}
-        </Listbox.Button>
-        <Listbox.Options
+      <Button
+        className={clsx([
+          "text-white",
+          "bg-blue",
+          "w-[70px]",
+          "rounded-[4px]",
+          "wide-phone:h-[30px]",
+        ])}
+        onClick={() => setSelectIsOpen((prevIsOpen) => !prevIsOpen)}
+      >
+        {languageSlugsToLabels[selectedLanguage]}
+        <CaretBottom
           className={clsx([
-            "hidden",
-            "absolute",
-            "wide-phone:top-[-5px]",
-            "wide-phone:left-[104px]",
-            "bg-white",
-            "wide-phone:flex",
-            "flex-row",
-            "tablet:flex-col",
-            "mt-[5px]",
-            "gap-[5px]",
-            "px-[10px]",
-            "pb-[10px]",
-            "left-[-10px]",
-            "rounded-[20px]",
+            "h-5",
+            "w-5",
+            isOpen && "rotate-180",
+            isOpen && "tablet:rotate-[270deg]",
           ])}
-        >
-          {Object.keys(languageSlugsToLabels)
-            .filter((slug) => slug !== selectedLanguage)
-            .map((slug) => (
-              <Listbox.Option value={slug} key={slug}>
-                <Button
-                  className={clsx([
-                    "w-[110px]",
-                    "shadow-button",
-                    "tablet:shadow-button-tablet",
-                    "rounded-[4px]",
-                    "hover:text-white",
-                    "hover:bg-blue",
-                  ])}
-                >
-                  {languageSlugsToLabels[slug as LanguageSlug]}
-                </Button>
-              </Listbox.Option>
-            ))}
-        </Listbox.Options>
-      </Listbox>
+          fill="#FFFFFF"
+        />
+      </Button>
     </div>
   );
 }
--- docs/src/components/Navigation/Navigation.tsx
+++ docs/src/components/Navigation/Navigation.tsx
@@ -1,10 +1,15 @@
-import { LanguageSlug, getPathParts } from "@/lib/languageFromPath";
+import {
+  LanguageSlug,
+  getPathParts,
+  languageSlugsToLabels,
+} from "@/lib/languageFromPath";
 import clsx from "clsx";
 import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
 import { Button } from "../Button";
 import { Selfie } from "../Selfie";
 import { LanguageSelect } from "./LanguageSelect";
+import { useState } from "react";
 
 export function Navigation() {
   const router = useRouter();
@@ -18,9 +23,40 @@ export function Navigation() {
     router.push(nextRoute);
   }
 
+  const [selectIsOpen, setSelectIsOpen] = useState<boolean>(false);
+
   return (
-    <div className={clsx(["flex", "justify-between", "gap-[10px]"])}>
-      <div className={clsx(["flex", "items-end", "gap-[10px]"])}>
+    <div
+      className={clsx([
+        "relative",
+        "flex",
+        "justify-between",
+        "gap-[10px]",
+        "z-10",
+        "overflow-hidden",
+        "mb-[-4px]",
+        "pb-[4px]",
+      ])}
+    >
+      <div
+        className={clsx([
+          "absolute",
+          "bg-white",
+          "w-[220px]",
+          "h-full",
+          "bottom-[-4px]",
+          "z-10",
+        ])}
+      ></div>
+      <div
+        className={clsx([
+          "flex",
+          "items-end",
+          "gap-[10px]",
+          "z-20",
+          "bg-white",
+        ])}
+      >
         <Link href={"/"}>
           <Selfie
             className={clsx([
@@ -34,10 +70,52 @@ export function Navigation() {
         </Link>
         <LanguageSelect
           selectedLanguage={pathParts.language}
-          handleChange={handleChange}
+          setSelectIsOpen={setSelectIsOpen}
+          isOpen={selectIsOpen}
         />
       </div>
-      <nav className={clsx(["flex", "items-end", "justify-end"])}>
+      <div
+        className={clsx([
+          "hidden",
+          "absolute",
+          "bg-white",
+          "bottom-[4px]",
+          "wide-phone:flex",
+          "wide-phone:gap-[10px]",
+          "transition-all",
+          "duration-500",
+          selectIsOpen ? "translate-x-[230px]" : "translate-x-[-20px]",
+        ])}
+      >
+        {Object.keys(languageSlugsToLabels)
+          .filter((slug) => slug !== pathParts.language)
+          .map((slug) => (
+            <Button
+              className={clsx([
+                "w-[70px]",
+                "shadow-button",
+                "tablet:shadow-button-tablet",
+                "rounded-[4px]",
+                "hover:text-white",
+                "hover:bg-blue",
+              ])}
+              onClick={() => handleChange(slug as LanguageSlug)}
+              key={slug}
+            >
+              {languageSlugsToLabels[slug as LanguageSlug]}
+            </Button>
+          ))}
+      </div>
+      <nav
+        className={clsx([
+          "flex",
+          "items-end",
+          "justify-end",
+          "transition-all",
+          "duration-500",
+          selectIsOpen ? "translate-x-[280px]" : "translate-x-0",
+        ])}
+      >
         <ul role="list">
           <li className={clsx(["flex", "gap-[10px]"])}>
             <Link
@@ -99,9 +177,9 @@ export function Navigation() {
   );
 }
 
-const pressedContainerClasses = clsx(["pl-[4px]", "pt-[2px]"]);
+const pressedContainerClasses = clsx(["pt-[2px]"]);
 
-const unPressedContainerClasses = clsx(["pr-[4px]"]);
+const unPressedContainerClasses = clsx([""]);
 
 const sharedClasses = clsx([
   "px-1",
@@ -109,16 +187,17 @@ const sharedClasses = clsx([
   "justify-center",
   "items-center",
   "cursor-pointer",
-  "h-[23px]",
   "text-[16px]",
   "border",
+  "h-[23px]",
   "border-black",
   "border-2",
   "rounded-[4px]",
   "hover:text-white",
   "hover:bg-blue",
+  "wide-phone:text-base",
+  "wide-phone:h-[30px]",
   "tablet:h-[35px]",
-  "tablet:text-[22px]",
   "tablet:border-[3px]",
   "tablet:rounded-[10px]",
   "desktop:h-[53px]",
