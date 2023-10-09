96f7619e6c4bfa47158ba0214c146be629a5aeee
toddriley <toddriley@todds-mbp-2>
2023-10-09 15:44:17 -0400

    Try to recalculate the variables when buttons are clicked.

--- gradle/dokka/scripts/selfie.js
+++ gradle/dokka/scripts/selfie.js
@@ -1,13 +1,27 @@
 function selfieMain() {
     const main = document.getElementById("main");
-    document.body.style.setProperty("--content-height", main.offsetHeight + "px")
-    document.body.style.setProperty("--selfie-bot", "-" + main.scrollTop + "px")
+
+    function calculateContentHeight() {
+        document.body.style.setProperty("--content-height", main.offsetHeight + "px")
+        document.body.style.setProperty("--selfie-bot", "-" + main.scrollTop + "px")
+    }
+
+    calculateContentHeight()
+
+    function delayAndCalculateContentHeight() {
+        setTimeout(calculateContentHeight, 100)
+    }
 
     function onContentScroll(e) {
         document.body.style.setProperty("--selfie-bot", "-" + e.target.scrollTop + "px")
     }
 
     main.addEventListener("scroll", onContentScroll, false);
+
+    document.querySelectorAll("#filter-section > button")
+        .forEach(f => {
+            f.addEventListener("click", delayAndCalculateContentHeight)
+        })
 }
 
 window.addEventListener("load", selfieMain)
