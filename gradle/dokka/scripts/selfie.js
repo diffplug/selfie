c0e35a9e23268f910596357286c9c83ddecc6d6c
toddriley <toddriley@todds-mbp-2>
2023-10-09 16:19:58 -0400

    Stick my guy in the footer.

--- gradle/dokka/scripts/selfie.js
+++ gradle/dokka/scripts/selfie.js
@@ -1,27 +1,12 @@
 function selfieMain() {
     const main = document.getElementById("main");
-
-    function calculateContentHeight() {
-        document.body.style.setProperty("--content-height", main.offsetHeight + "px")
-        document.body.style.setProperty("--selfie-bot", "-" + main.scrollTop + "px")
-    }
-
-    calculateContentHeight()
-
-    function delayAndCalculateContentHeight() {
-        setTimeout(calculateContentHeight, 100)
-    }
+    document.body.style.setProperty("--selfie-bot", "-" + main.scrollTop + "px")
 
     function onContentScroll(e) {
         document.body.style.setProperty("--selfie-bot", "-" + e.target.scrollTop + "px")
     }
 
     main.addEventListener("scroll", onContentScroll, false);
-
-    document.querySelectorAll("#filter-section > button")
-        .forEach(f => {
-            f.addEventListener("click", delayAndCalculateContentHeight)
-        })
 }
 
 window.addEventListener("load", selfieMain)
