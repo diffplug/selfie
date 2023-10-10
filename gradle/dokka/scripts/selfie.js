f9434302fed2e423b3e924f0609733a5e3d6fba2
toddriley <toddriley@todds-mbp-2>
2023-10-10 15:42:44 -0400

    Recalculate bot offset on resize.

--- gradle/dokka/scripts/selfie.js
+++ gradle/dokka/scripts/selfie.js
@@ -3,20 +3,21 @@ const SELFIE_BOT_HEIGHT = 300; // matches height in stylesheet
 function selfieMain() {
     const main = document.getElementById("main");
     const bot = document.getElementById("selfie-navigation-bot");
-    let initialBotOffset = 0;
+    let baseBotOffset = 0;
 
     function onContentScroll(e) {
-        bot.style.setProperty("--selfie-bot", "" + initialBotOffset - e.target.scrollTop + "px");
+        bot.style.setProperty("--selfie-bot", "" + baseBotOffset - e.target.scrollTop + "px");
     }
 
-    function initializeBotOffset() {
+    function calculateBotOffset() {
         // The bottom of the selfie bot should align with the top of the first visible code block
         const codeBlocks = Array.from(main.querySelectorAll(".symbol"));
         codeBlocks.some((cb, idx) => {
-            initialBotOffset = cb.offsetTop - SELFIE_BOT_HEIGHT;
+            const box = cb.getBoundingClientRect()
+            baseBotOffset = box.top - SELFIE_BOT_HEIGHT;
             return cb.offsetParent !== null;
         })
-        bot.style.setProperty("--selfie-bot", "" + initialBotOffset - main.scrollTop + "px");
+        bot.style.setProperty("--selfie-bot", "" + baseBotOffset - main.scrollTop + "px");
         fadeBotIn();
     }
 
@@ -24,10 +25,15 @@ function selfieMain() {
         bot.style.setProperty("--selfie-opacity", "1");
         bot.style.opacity = "var(--selfie-opacity)";
     }
-    
-    initializeBotOffset();
+
+    calculateBotOffset();
 
     main.addEventListener("scroll", onContentScroll, false);
+    let timeout;
+    window.onresize = function(){
+        clearTimeout(timeout);
+        timeout = setTimeout(calculateBotOffset, 100);
+    };
 }
 
 window.addEventListener("load", selfieMain);
