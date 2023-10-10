09433c4b3a96f1771f517020dc6ded6ba21871eb
toddriley <toddriley@todds-mbp-2>
2023-10-10 16:12:35 -0400

    Fine tune bot positioning on different pages.

--- gradle/dokka/scripts/selfie.js
+++ gradle/dokka/scripts/selfie.js
@@ -12,18 +12,29 @@ function selfieMain() {
     function calculateBotOffset() {
         // The bottom of the selfie bot should align with the top of the first visible code block
         const codeBlocks = Array.from(main.querySelectorAll(".symbol"));
-        codeBlocks.some((cb, idx) => {
-            const box = cb.getBoundingClientRect()
-            baseBotOffset = box.top - SELFIE_BOT_HEIGHT;
-            return cb.offsetParent !== null;
+        const isFound = codeBlocks.some(function(cb) {
+            const parent = cb.parentElement;
+            const parentHasBorder = hasBorder(parent);
+            bot.style.marginRight = parentHasBorder ? "4px" : "0";
+            const element = parentHasBorder ? parent : cb;
+            baseBotOffset = element.getBoundingClientRect().top - SELFIE_BOT_HEIGHT;
+            return element.offsetParent !== null;
         })
-        bot.style.setProperty("--selfie-bot", "" + baseBotOffset - main.scrollTop + "px");
-        fadeBotIn();
+        if (isFound) {
+            bot.style.setProperty("--selfie-bot", "" + baseBotOffset - main.scrollTop + "px");
+            fadeBotIn();
+        } else {
+            hideBot();
+        }
     }
 
     function fadeBotIn() {
         bot.style.setProperty("--selfie-opacity", "1");
         bot.style.opacity = "var(--selfie-opacity)";
+    }
+
+    function hideBot() {
+        bot.style.setProperty("--selfie-opacity", "0");
     }
 
     calculateBotOffset();
@@ -32,8 +43,12 @@ function selfieMain() {
     let timeout;
     window.onresize = function(){
         clearTimeout(timeout);
-        timeout = setTimeout(calculateBotOffset, 100);
+        timeout = setTimeout(calculateBotOffset, 50);
     };
 }
 
 window.addEventListener("load", selfieMain);
+
+function hasBorder(element) {
+    return element.matches(".cover > .with-platform-tabs > .content");
+}
