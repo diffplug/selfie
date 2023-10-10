91a481fe23243b363cad92ad424d00d44b54c073
toddriley <toddriley@todds-mbp-2>
2023-10-10 11:18:14 -0400

    Align bot with top of first code block on page load.

--- gradle/dokka/scripts/selfie.js
+++ gradle/dokka/scripts/selfie.js
@@ -1,10 +1,24 @@
+const SELFIE_BOT_HEIGHT = 300; // matches height in stylesheet
+
 function selfieMain() {
     const main = document.getElementById("main");
-    document.body.style.setProperty("--selfie-bot", "-" + main.scrollTop + "px")
+    let initialBotOffset = 0;
 
     function onContentScroll(e) {
-        document.body.style.setProperty("--selfie-bot", "-" + e.target.scrollTop + "px")
+        document.body.style.setProperty("--selfie-bot", "" + initialBotOffset - e.target.scrollTop + "px")
     }
+
+    function initializeBotOffset() {
+        // The bottom of the selfie bot should align with the top of the first visible code block
+        const codeBlocks = Array.from(main.querySelectorAll(".symbol"));
+        codeBlocks.some((cb, idx) => {
+            initialBotOffset = cb.offsetTop - SELFIE_BOT_HEIGHT;
+            return cb.offsetParent !== null
+        })
+        document.body.style.setProperty("--selfie-bot", "" + initialBotOffset - main.scrollTop + "px")
+    }
+
+    initializeBotOffset();
 
     main.addEventListener("scroll", onContentScroll, false);
 }
