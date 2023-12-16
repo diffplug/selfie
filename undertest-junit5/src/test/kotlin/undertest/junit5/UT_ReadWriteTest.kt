d26c9ddff9e4293c74b5ff8a45102882607e92e7
Ned Twigg <ned.twigg@diffplug.com>
2023-12-16 15:05:37 -0800

    Handle the `selfie` -> `Selfie` name change.

--- undertest-junit5/src/test/kotlin/undertest/junit5/UT_ReadWriteTest.kt
+++ undertest-junit5/src/test/kotlin/undertest/junit5/UT_ReadWriteTest.kt
@@ -1,11 +1,11 @@
 package undertest.junit5
 
-import com.diffplug.selfie.expectSelfie
+import com.diffplug.selfie.Selfie.expectSelfie
 import kotlin.test.Test
 
 class UT_ReadWriteTest {
   @Test fun selfie() {
-//    expectSelfie("apple").toMatchDisk()
-    expectSelfie("orange").toMatchDisk()
+    expectSelfie("apple").toMatchDisk()
+//    expectSelfie("orange").toMatchDisk()
   }
 }
--- undertest-junit5/src/test/kotlin/undertest/junit5/UT_WithinMethodGCTest.kt
+++ undertest-junit5/src/test/kotlin/undertest/junit5/UT_WithinMethodGCTest.kt
@@ -1,6 +1,6 @@
 package undertest.junit5
 // spotless:off
-import com.diffplug.selfie.expectSelfie
+import com.diffplug.selfie.Selfie.expectSelfie
 import kotlin.test.Test
 
 // spotless:on
@@ -9,6 +9,6 @@ class UT_WithinMethodGCTest {
 //    @Test fun selfie2() {
   @Test fun selfie() {
     expectSelfie("root").toMatchDisk()
-    expectSelfie("maple").toMatchDisk("leaf")
+//    expectSelfie("maple").toMatchDisk("leaf")
   }
 }
