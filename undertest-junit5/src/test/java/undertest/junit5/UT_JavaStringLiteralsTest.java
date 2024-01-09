7cd0789fa0677c9aa34e600bd015b27de413ce33
Ned Twigg <ned.twigg@diffplug.com>
2024-01-08 22:51:29 -0800

    Add a test for various challenging string literals.

---
+++ undertest-junit5/src/test/java/undertest/junit5/UT_JavaStringLiteralsTest.java
@@ -0,0 +1,35 @@
+package undertest.junit5;
+
+import org.junit.jupiter.api.Test;
+import static com.diffplug.selfie.Selfie.expectSelfie;
+
+public class UT_JavaStringLiteralsTest {
+    @Test
+    public void empty() {
+        expectSelfie("").toBe_TODO();
+    }
+
+    @Test
+    public void tabs() {
+        expectSelfie("\t\t\t").toBe_TODO();
+    }
+
+    @Test
+    public void spaces() {
+        expectSelfie("   ").toBe_TODO();
+    }
+
+    @Test
+    public void newlines() {
+        expectSelfie("\n").toBe_TODO();
+        expectSelfie("\n\n").toBe_TODO();
+        expectSelfie("\n\n\n").toBe_TODO();
+    }
+
+    @Test
+    public void allOfIt() {
+        expectSelfie("  a\n" +
+                "a  \n" +
+                "\t a \t\n").toBe_TODO();
+    }
+}
