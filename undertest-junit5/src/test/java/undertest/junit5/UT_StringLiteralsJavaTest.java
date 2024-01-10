47f035d9808cca5681718bc093c8b4d0130c99b6
Ned Twigg <ned.twigg@diffplug.com>
2024-01-09 22:31:57 -0800

    Make the string literals test more challenging.

--- undertest-junit5/src/test/java/undertest/junit5/UT_StringLiteralsJavaTest.java
+++ undertest-junit5/src/test/java/undertest/junit5/UT_StringLiteralsJavaTest.java
@@ -6,30 +6,51 @@ import static com.diffplug.selfie.Selfie.expectSelfie;
 public class UT_StringLiteralsJavaTest {
     @Test
     public void empty() {
-        expectSelfie("").toBe_TODO();
+        expectSelfie("").toBe("");
     }
 
     @Test
     public void tabs() {
-        expectSelfie("\t\t\t").toBe_TODO();
+        expectSelfie("\t\t\t").toBe("\t\t\t");
     }
 
     @Test
     public void spaces() {
-        expectSelfie("   ").toBe_TODO();
+        expectSelfie("   ").toBe("   ");
     }
 
     @Test
     public void newlines() {
-        expectSelfie("\n").toBe_TODO();
-        expectSelfie("\n\n").toBe_TODO();
-        expectSelfie("\n\n\n").toBe_TODO();
+        expectSelfie("\n").toBe("""
+
+""");
+        expectSelfie("\n\n").toBe("""
+
+
+""");
+        expectSelfie("\n\n\n").toBe("""
+
+
+
+""");
+    }
+
+    @Test
+    public void escapableCharacters() {
+        expectSelfie(" ' \" $ ").toBe(" ' \" $ ");
+        expectSelfie(" ' \" $ \n \"\"\"\"\"\"\"\"\"\t").toBe("""
+\s' " $\s
+\s\"\"\"\"\"\"\"\"\"\t""");
     }
 
     @Test
     public void allOfIt() {
         expectSelfie("  a\n" +
                 "a  \n" +
-                "\t a \t\n").toBe_TODO();
+                "\t a \t\n").toBe("""
+\s a
+a \s
+\t a \t
+""");
     }
 }
