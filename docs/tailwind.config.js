95823173c2830355d009bafa1198909a8396520a
Todd Riley <todd@websbytodd.com>
2024-01-15 14:58:57 -0500

    Include ts files.

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,mjs,jsx,mdx,tsx}"],
+  content: ["./src/**/*.{js,mjs,jsx,mdx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
