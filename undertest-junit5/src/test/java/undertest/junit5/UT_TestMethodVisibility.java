f95f19393e59e52aeee96f41d149e4189867d48f
Ned Twigg <ned.twigg@diffplug.com>
2024-01-25 15:01:10 -0800

    And it stays fixed even if the class itself doesn't declare any tests.

--- undertest-junit5/src/test/java/undertest/junit5/UT_TestMethodVisibility.java
+++ undertest-junit5/src/test/java/undertest/junit5/UT_TestMethodVisibility.java
@@ -5,17 +5,8 @@ import static com.diffplug.selfie.Selfie.expectSelfie;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class UT_TestMethodVisibility {
-    @Test
-    void isPackage() {
-        expectSelfie("package").toMatchDisk();
-    }
-
-    @Test
-    protected void isProtected() {
-        expectSelfie("protected").toMatchDisk();
-    }
-
+public class UT_TestMethodVisibility extends UT_TestMethodVisibilityParentClass {
+    // get test methods from parent class
     @Test
     private void isPrivate() {
         Assertions.fail("Test methods can't be private");
