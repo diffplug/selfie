6cc0de794cc0e13364bb805007dd178851939e40
Ned Twigg <ned.twigg@diffplug.com>
2024-02-05 14:49:49 -0800

    Bash on our concurrency model.

---
+++ jvm/selfie-runner-junit5/src/test/kotlin/com/diffplug/selfie/kotest/KotestConcurrencyStressTest.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2024 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.selfie.kotest
+
+import com.diffplug.selfie.ArrayMap
+import com.diffplug.selfie.Snapshot
+import com.diffplug.selfie.SnapshotFile
+import com.diffplug.selfie.junit5.HarnessJUnit
+import kotlin.test.Test
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.TestMethodOrder
+import org.junitpioneer.jupiter.DisableIfTestFails
+
+@Tag("kotest")
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@DisableIfTestFails
+class KotestConcurrencyStressTest : HarnessJUnit() {
+  @Test @Order(1)
+  fun initialState() {
+    var expectedSnapshots = ArrayMap.empty<String, Snapshot>()
+    for (d in 1..1000) {
+      expectedSnapshots =
+          expectedSnapshots.plus(String.format("test %04d", d), Snapshot.of(d.toString()))
+    }
+    val snapshotFile = SnapshotFile()
+    snapshotFile.snapshots = expectedSnapshots
+    val buffer = StringBuilder()
+    snapshotFile.serialize(buffer)
+
+    gradleWriteSS()
+    ut_snapshot().assertContent(buffer.toString())
+    gradleReadSS()
+    ut_snapshot().deleteIfExists()
+  }
+}
