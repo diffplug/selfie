64f3973aa45ba64f3ea51e721952c18ad605705e
Ned Twigg <ned.twigg@diffplug.com>
2024-02-05 14:21:49 -0800

    Doublecheck that FunSpec works too.

---
+++ jvm/undertest-junit5-kotest/src/test/kotlin/undertest/junit5/UT_KotestFunSpecTest.kt
@@ -0,0 +1,11 @@
+package undertest.junit5
+
+import com.diffplug.selfie.Selfie.expectSelfie
+import io.kotest.core.spec.style.FunSpec
+
+class UT_KotestFunSpecTest :
+    FunSpec({
+      test("a") { expectSelfie("a").toMatchDisk() }
+      test("b") { expectSelfie("b").toMatchDisk() }
+      test("c") { expectSelfie("c").toMatchDisk() }
+    })
