7ad530442a57dbea1ee7fe455caa0e23a2dd9942
Ned Twigg <ned.twigg@diffplug.com>
2024-02-07 21:29:26 -0800

    Match selfie-runner-kotest to selfie-lib, and fixes our weird reflection thing! Huzzah!!

--- jvm/undertest-kotest/src/jvmTest/kotlin/kotest/KotestProjectConfig.kt
+++ jvm/undertest-kotest/src/jvmTest/kotlin/kotest/KotestProjectConfig.kt
@@ -1,13 +1,7 @@
 package kotest
 
+import com.diffplug.selfie.kotest.SelfieExtension
 import io.kotest.core.config.AbstractProjectConfig
-import io.kotest.core.extensions.Extension
-// it's super weird that we can't just say `com.diffplug.selfie.kotest.SelfieExtension`
-// pretty sure it's a bug in the Kotlin multiplatform plugin
-val SelfieExtension =
-    Class.forName("com.diffplug.selfie.kotest.SelfieExtension").let {
-      it.getField("INSTANCE").get(null)
-    } as Extension
 
 object KotestProjectConfig : AbstractProjectConfig() {
   override fun extensions() = listOf(SelfieExtension)
