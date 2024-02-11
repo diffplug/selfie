93046f2c36869af4e31453c83da6e73452622687
Ned Twigg <ned.twigg@diffplug.com>
2024-02-11 12:27:13 -0800

    Test error message for using `coroutines` in regular JUnit.

---
+++ jvm/undertest-junit5/src/test/kotlin/undertest/junit5/UT_WrongContextInJUnit.kt
@@ -0,0 +1,24 @@
+package undertest.junit5
+
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+
+class UT_WrongContextInJUnit {
+  @Test fun wrongContext() {
+    try {
+      runBlocking { com.diffplug.selfie.coroutines.expectSelfie("something").toMatchDisk() }
+    } catch (e: Throwable) {
+      com.diffplug.selfie.Selfie.expectSelfie(e.message!!)
+          .toBe(
+              """No Kotest test is in progress on this coroutine.
+If this is a Kotest test, make sure you added `SelfieExtension` to your `AbstractProjectConfig`:
+${' '} +class MyProjectConfig : AbstractProjectConfig() {
+${' '} +  override fun extensions() = listOf(SelfieExtension(this))
+${' '} +}
+If this is a JUnit test, make the following change:
+${' '} -import com.diffplug.selfie.coroutines.expectSelfie
+${' '} +import com.diffplug.selfie.Selfie.expectSelfie
+For more info https://selfie.dev/jvm/kotest#selfie-and-coroutines""")
+    }
+  }
+}
