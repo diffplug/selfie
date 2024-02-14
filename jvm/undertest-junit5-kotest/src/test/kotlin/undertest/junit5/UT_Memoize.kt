7efed42dab9bdd602eacaf299eeaddf2905665a0
Ned Twigg <ned.twigg@diffplug.com>
2024-02-14 14:54:53 -0800

    Make the test more thorough.

--- jvm/undertest-junit5-kotest/src/test/kotlin/undertest/junit5/UT_Memoize.kt
+++ jvm/undertest-junit5-kotest/src/test/kotlin/undertest/junit5/UT_Memoize.kt
@@ -4,12 +4,17 @@ import com.diffplug.selfie.coroutines.memoize
 import com.diffplug.selfie.coroutines.memoizeAsJson
 import com.diffplug.selfie.coroutines.memoizeBinarySerializable
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 
 class UT_Memoize :
     FunSpec({
       test("java.io.Serializable") {
-        memoizeBinarySerializable { Book("Harry Potter", "J.K. Rowling") }.toBeFile_TODO("Book.bin")
-        memoizeBinarySerializable { Book("Moby Dick", "Herman Melville") }.toBeBase64_TODO()
+        memoizeBinarySerializable { Book("Harry Potter", "J.K. Rowling") }
+            .toBeFile_TODO("Book.bin")
+            .run { title shouldBe "Harry Potter" }
+        memoizeBinarySerializable { Book("Moby Dick", "Herman Melville") }
+            .toBeBase64_TODO()
+            .run { title shouldBe "Moby Dick" }
       }
       test("kotlinx.serialization.Serializable") {
         memoizeAsJson { Book("Cat in the Hat", "Dr. Seuss") }
@@ -17,6 +22,7 @@ class UT_Memoize :
 ${' '} "title": "Cat in the Hat",
 ${' '} "author": "Dr. Seuss"
 }""")
+            .run { title shouldBe "Cat in the Hat" }
       }
       test("nanoTimeTest") {
 // easy way to test if it's memoizing or running every time
