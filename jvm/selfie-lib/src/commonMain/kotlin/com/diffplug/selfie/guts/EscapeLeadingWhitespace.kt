2c5be0324fb97f77084fcbcdc8a9a20ce0478cad
Ned Twigg <ned.twigg@diffplug.com>
2024-02-20 16:11:29 -0800

    No need for a regex.

--- jvm/selfie-lib/src/commonMain/kotlin/com/diffplug/selfie/guts/EscapeLeadingWhitespace.kt
+++ jvm/selfie-lib/src/commonMain/kotlin/com/diffplug/selfie/guts/EscapeLeadingWhitespace.kt
@@ -28,18 +28,16 @@ internal enum class EscapeLeadingWhitespace {
       } else line
 
   companion object {
-    private val leadingWhitespaceRegex = "^\\s+".toRegex()
     private val MIXED = 'm'
     fun appropriateFor(fileContent: String): EscapeLeadingWhitespace {
       val commonWhitespace =
           fileContent
               .lineSequence()
               .mapNotNull { line ->
-                leadingWhitespaceRegex.find(line)?.value?.let { whitespace ->
-                  if (whitespace.isEmpty()) null
-                  else if (whitespace.all { it == ' ' }) ' '
-                  else if (whitespace.all { it == '\t' }) '\t' else MIXED
-                }
+                val whitespace = line.takeWhile { it.isWhitespace() }
+                if (whitespace.isEmpty()) null
+                else if (whitespace.all { it == ' ' }) ' '
+                else if (whitespace.all { it == '\t' }) '\t' else MIXED
               }
               .reduceOrNull { a, b -> if (a == b) a else MIXED }
       return if (commonWhitespace == ' ') ONLY_ON_TAB
