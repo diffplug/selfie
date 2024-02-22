2005fdbdc0e32eb2adc067fedc8049487938918f
Edwin <eye@dons.usfca.edu>
2024-02-22 12:34:22 -0800

    Make changes to assigned in python

--- python/selfie-lib/selfie_lib/LineReader.py
+++ python/selfie-lib/selfie_lib/LineReader.py
@@ -1,48 +1,48 @@
-from io import StringIO, BufferedReader, TextIOWrapper
 import io
 
-class LineTerminatorReader(BufferedReader):
+class LineTerminatorAware:
     def __init__(self, reader):
-        super().__init__(reader)
-        self.unix_newlines = True
-
-    def read(self, size=-1):
-        chunk = super().read(size)
-        if '\r' in chunk:
-            self.unix_newlines = False
-        return chunk
-
-    def read_line(self):
-        line = super().readline()
-        if '\r' in line:
-            self.unix_newlines = False
-        return line.rstrip('\n').rstrip('\r')
-
-    def unix_newlines(self):
-        return self.unix_newlines
-
-class LineReader:
-    def __init__(self, reader):
-        self.reader = LineTerminatorReader(reader)
+        self.reader = reader
         self.line_number = 0
-
-    @classmethod
-    def for_string(cls, content):
-        return cls(StringIO(content))
-
-    @classmethod
-    def for_binary(cls, content):
-        return cls(TextIOWrapper(io.BytesIO(content), encoding='utf-8'))
-
-    def get_line_number(self):
-        return self.line_number
+        self.unix_newlines = True
+        self.first_line = self.reader.readline()
+        if '\r' in self.first_line:
+            self.unix_newlines = False
 
     def read_line(self):
-        line = self.reader.read_line()
+        if self.first_line:
+            line = self.first_line
+            self.first_line = None
+            self.line_number += 1
+            return line
+        line = self.reader.readline()
         if line:
+            if '\r' in line:
+                self.unix_newlines = False
             self.line_number += 1
         return line
 
     def unix_newlines(self):
-        return self.reader.unix_newlines()
+        return self.unix_newlines
 
+
+class LineReader:
+    def __init__(self, reader):
+        self.reader = LineTerminatorAware(reader)
+
+    @classmethod
+    def for_string(cls, content):
+        return cls(io.StringIO(content))
+
+    @classmethod
+    def for_binary(cls, content):
+        return cls(io.BytesIO(content).read().decode('utf-8'))
+
+    def get_line_number(self):
+        return self.reader.line_number
+
+    def read_line(self):
+        return self.reader.read_line()
+
+    def unix_newlines(self):
+        return self.reader.unix_newlines()
