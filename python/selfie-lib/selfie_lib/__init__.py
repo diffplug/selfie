041af0272ef1b3f8c5bdb10602d882916f0482c4
Edwin <eye@dons.usfca.edu>
2024-02-22 00:22:03 -0800

    Resolved merge conflicts

--- python/selfie-lib/selfie_lib/__init__.py
+++ python/selfie-lib/selfie_lib/__init__.py
@@ -1,3 +1,3 @@
 from .ned import fizzbuzz as fizzbuzz
 from .harvir import silly_addition as silly_addition
-from .edwin import silly_subtraction as silly_subtraction
+from .edwin import simple_subtraction as simple_subtraction
