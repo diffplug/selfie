74a22fb09eb1b69fbff22cc0cb7444d22532a517
Edwin <eye@dons.usfca.edu>
2024-02-27 10:26:33 -0800

    importing LineReader at init

--- python/selfie-lib/selfie_lib/__init__.py
+++ python/selfie-lib/selfie_lib/__init__.py
@@ -2,4 +2,3 @@ from .ned import fizzbuzz as fizzbuzz
 from .harvir import silly_addition as silly_addition
 from .edwin import simple_subtraction as simple_subtraction
 from .LineReader import LineReader as LineReader
-
