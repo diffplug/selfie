4596ba9c01830e5d7dc7de4f625a44d5da21de35
Edwin <eye@dons.usfca.edu>
2024-02-27 14:10:49 -0800

    Make changes to LineReader and rename LineReader_test

--- python/selfie-lib/selfie_lib/LineReader.py
+++ python/selfie-lib/selfie_lib/LineReader.py
@@ -1,54 +1,32 @@
 import io
-
-class LineTerminatorAware(io.BufferedReader):
-    def __init__(self, buffer):
-        super().__init__(buffer)
-        self.first_line = self.readline()
-        self.unix_newlines = True
-
-    def readline(self, *args, **kwargs):
-        if self.first_line is not None:
-            result = self.first_line
-            self.first_line = None
-            return result
-        return super().readline(*args, **kwargs)
+from typing import Union
 
 class LineReader:
-    def __init__(self, reader):
-        self.reader = LineTerminatorAware(LineTerminatorReader(reader))
+    def __init__(self, content: Union[bytes, str]):
+        if isinstance(content, str):
+            content = content.encode('utf-8')
+        self.buffer = io.BytesIO(content)
+        self.uses_unix_newlines = self.detect_newline_type()
+
+    def detect_newline_type(self) -> bool:
+        first_line = self.buffer.readline()
+        # Reset buffer after checking the first line
+        self.buffer.seek(0)
+        return b'\r\n' not in first_line
 
     @classmethod
-    def for_string(cls, content):
-        return cls(io.StringIO(content))
+    def for_binary(cls, content: bytes):
+        return cls(content)
 
     @classmethod
-    def for_binary(cls, content):
-        return cls(io.BytesIO(content).read().decode('utf-8'))
+    def for_string(cls, content: str):
+        return cls(content)
 
-    def get_line_number(self):
-        # Python's io module does not track line numbers directly.
-        # This functionality would need to be implemented manually if required.
-        pass
+    def unix_newlines(self) -> bool:
+        return self.uses_unix_newlines
 
-    def read_line(self):
-        return self.reader.readline()
+    def read_line(self) -> str:
+        line = self.buffer.readline().decode('utf-8')
+        return line.rstrip('\r\n')
 
-    def unix_newlines(self):
-        return self.reader.unix_newlines
 
-class LineTerminatorReader(io.StringIO):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.unix_newlines = True
-
-    def read(self, *args, **kwargs):
-        result = super().read(*args, **kwargs)
-        if '\r' in result:
-            self.unix_newlines = False
-        return result
-
-    def readline(self, *args, **kwargs):
-        line = super().readline(*args, **kwargs)
-        if '\r' in line:
-            self.unix_newlines = False
-        return line
