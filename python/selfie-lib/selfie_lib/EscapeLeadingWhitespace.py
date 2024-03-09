941d91dcbb0e97a48c54f008b155d97ed4ed69cd
Selina Delgado <sdelgado411@gmail.com>
2024-03-08 19:09:01 -0800

    fixed the enum problem

--- python/selfie-lib/selfie_lib/EscapeLeadingWhitespace.py
+++ python/selfie-lib/selfie_lib/EscapeLeadingWhitespace.py
@@ -1,11 +1,8 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class EscapeLeadingWhitespace(Enum):
-    NEVER = "NEVER"
-
-    def __init__(self, escape_type):
-        self.escape_type = escape_type
+    NEVER = auto()
 
     def escape_line(self, line: str, space: str, tab: str) -> str:
         return line
--- python/selfie-lib/selfie_lib/Literals.py
+++ python/selfie-lib/selfie_lib/Literals.py
@@ -1,8 +1,10 @@
-from enum import Enum
+from enum import Enum, auto
+from typing import Any
+from .EscapeLeadingWhitespace import EscapeLeadingWhitespace
 
 
 class Language(Enum):
-    PYTHON = "PYTHON"
+    PYTHON = auto()
 
     @classmethod
     def from_filename(cls, filename: str) -> "Language":
@@ -11,3 +13,34 @@ class Language(Enum):
             return cls.PYTHON
         else:
             raise ValueError(f"Unknown language for file {filename}")
+
+
+class LiteralValue:
+    def __init__(self, expected: Any, actual: Any, format: "LiteralFormat") -> None:
+        self.expected = expected
+        self.actual = actual
+        self.format = format
+
+
+class LiteralFormat:
+    def encode(
+        self, value: Any, language: Language, encoding_policy: "EscapeLeadingWhitespace"
+    ) -> str:
+        raise NotImplementedError("Subclasses must implement the encode method")
+
+    def parse(self, string: str, language: Language) -> Any:
+        raise NotImplementedError("Subclasses must implement the parse method")
+
+
+MAX_RAW_NUMBER = 1000
+PADDING_SIZE = len(str(MAX_RAW_NUMBER)) - 1
+
+
+class LiteralBoolean(LiteralFormat):
+    def encode(
+        self, value: bool, language: Language, encoding_policy: EscapeLeadingWhitespace
+    ) -> str:
+        return str(value)
+
+    def parse(self, string: str, language: Language) -> bool:
+        return string.lower() == "true"
