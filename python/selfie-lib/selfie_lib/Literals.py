abb6b5aa4738813eee2a3f2bd601e5a45413dc99
Selina Delgado <sdelgado411@gmail.com>
2024-03-12 22:08:36 -0700

    corrected literals type signatures

--- python/selfie-lib/selfie_lib/Literals.py
+++ python/selfie-lib/selfie_lib/Literals.py
@@ -1,6 +1,9 @@
 from enum import Enum, auto
-from typing import Any
+from typing import Protocol, TypeVar
+from abc import abstractmethod
 from .EscapeLeadingWhitespace import EscapeLeadingWhitespace
+
+T = TypeVar("T")
 
 
 class Language(Enum):
@@ -16,19 +19,21 @@ class Language(Enum):
 
 
 class LiteralValue:
-    def __init__(self, expected: Any, actual: Any, format: "LiteralFormat") -> None:
+    def __init__(self, expected: T | None, actual: T, format: "LiteralFormat") -> None:
         self.expected = expected
         self.actual = actual
         self.format = format
 
 
-class LiteralFormat:
+class LiteralFormat(Protocol[T]):
+    @abstractmethod
     def encode(
-        self, value: Any, language: Language, encoding_policy: "EscapeLeadingWhitespace"
+        self, value: T, language: Language, encoding_policy: "EscapeLeadingWhitespace"
     ) -> str:
         raise NotImplementedError("Subclasses must implement the encode method")
 
-    def parse(self, string: str, language: Language) -> Any:
+    @abstractmethod
+    def parse(self, string: str, language: Language) -> T:
         raise NotImplementedError("Subclasses must implement the parse method")
 
 
@@ -36,11 +41,20 @@ MAX_RAW_NUMBER = 1000
 PADDING_SIZE = len(str(MAX_RAW_NUMBER)) - 1
 
 
-class LiteralBoolean(LiteralFormat):
+class LiteralBoolean(LiteralFormat[bool]):
     def encode(
         self, value: bool, language: Language, encoding_policy: EscapeLeadingWhitespace
     ) -> str:
         return str(value)
 
     def parse(self, string: str, language: Language) -> bool:
-        return string.lower() == "true"
+        return to_boolean_strict(string)
+
+
+def to_boolean_strict(string: str) -> bool:
+    if string.lower() == "true":
+        return True
+    elif string.lower() == "false":
+        return False
+    else:
+        raise ValueError("String is not a valid boolean representation: " + string)
