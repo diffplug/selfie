f2afb6486c094151684feb30c7004cad10d6605d
Selina Delgado <sdelgado411@gmail.com>
2024-03-15 15:10:58 -0700

    made encode_underscores a private method of LiteralInt

--- python/selfie-lib/selfie_lib/Literals.py
+++ python/selfie-lib/selfie_lib/Literals.py
@@ -42,31 +42,30 @@ MAX_RAW_NUMBER = 1000
 PADDING_SIZE = len(str(MAX_RAW_NUMBER)) - 1
 
 
-def _encode_underscores(
-    buffer: io.StringIO, value: int, language: Language
-) -> io.StringIO:
-    if value >= MAX_RAW_NUMBER:
-        mod = value % MAX_RAW_NUMBER
-        left_padding = PADDING_SIZE - len(str(mod))
-        _encode_underscores(buffer, value // MAX_RAW_NUMBER, language)
-        buffer.write("_")
-        buffer.write("0" * left_padding)
-        buffer.write(str(mod))
-        return buffer
-    elif value < 0:
-        buffer.write("-")
-        _encode_underscores(buffer, abs(value), language)
-        return buffer
-    else:
-        buffer.write(str(value))
-        return buffer
-
-
 class LiteralInt(LiteralFormat[int]):
+    def _encode_underscores(
+        self, buffer: io.StringIO, value: int, language: Language
+    ) -> io.StringIO:
+        if value >= MAX_RAW_NUMBER:
+            mod = value % MAX_RAW_NUMBER
+            left_padding = PADDING_SIZE - len(str(mod))
+            self._encode_underscores(buffer, value // MAX_RAW_NUMBER, language)
+            buffer.write("_")
+            buffer.write("0" * left_padding)
+            buffer.write(str(mod))
+            return buffer
+        elif value < 0:
+            buffer.write("-")
+            self._encode_underscores(buffer, abs(value), language)
+            return buffer
+        else:
+            buffer.write(str(value))
+            return buffer
+
     def encode(
         self, value: int, language: Language, encoding_policy: EscapeLeadingWhitespace
     ) -> str:
-        return _encode_underscores(io.StringIO(), value, language).getvalue()
+        return self._encode_underscores(io.StringIO(), value, language).getvalue()
 
     def parse(self, string: str, language: Language) -> int:
         return int(string.replace("_", ""))
