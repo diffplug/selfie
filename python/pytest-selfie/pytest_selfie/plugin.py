fc452ca56e5ac99a73c9db73920588110780d9f0
Ned Twigg <ned.twigg@diffplug.com>
2024-04-02 14:12:41 -0700

    We've got something that runs before all tests, after all tests, and before/after each test.

--- python/pytest-selfie/pytest_selfie/plugin.py
+++ python/pytest-selfie/pytest_selfie/plugin.py
@@ -1,3 +1,4 @@
+from typing import Optional, Tuple
 import pytest
 
 
@@ -17,3 +18,33 @@ def pytest_addoption(parser):
 @pytest.fixture
 def bar(request):
     return request.config.option.dest_foo
+
+
+@pytest.hookimpl
+def pytest_sessionstart(session: pytest.Session):
+    print("SELFIE SESSION STARTED")
+    pass
+
+
+@pytest.hookimpl
+def pytest_sessionfinish(session: pytest.Session, exitstatus):
+    print("SELFIE SESSION FINISHED")
+    pass
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_pyfunc_call(pyfuncitem: pytest.Function):
+    # do_something_before_next_hook_executes()
+    print("SELFIE start test {pytfuncitem.nodeid}")
+
+    outcome = yield
+
+    res = outcome.get_result()  # will raise if outcome was exception
+
+    print("SELFIE end test {pytfuncitem.nodeid} with {Res}")
+    # outcome.excinfo may be None or a (cls, val, tb) tuple
+    # res = outcome.get_result()  # will raise if outcome was exception
+
+    # post_process_result(res)
+
+    # outcome.force_result(new_res)  # to override the return value to the plugin system
