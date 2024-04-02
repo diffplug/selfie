4d4dcf14d65671b9f1c135922bb949406259e8ff
Selina Delgado <sdelgado411@gmail.com>
2024-04-01 21:34:47 -0700

    changed java names and fixed encode multi test

--- python/selfie-lib/tests/LiteralString_test.py
+++ python/selfie-lib/tests/LiteralString_test.py
@@ -8,7 +8,7 @@ class TestLiteralString:
         "value, expected",
         [("1", '"1"'), ("\\", '"\\\\"'), ("1\n\tABC", '"1\\n\\tABC"')],
     )
-    def test_encode_single_java(self, value, expected):
+    def test_encode_single(self, value, expected):
         literal_string = LiteralString()
         actual = literal_string._encodeSinglePython(value)
         print(actual)
@@ -18,7 +18,7 @@ class TestLiteralString:
         "value, expected",
         [("1", "`1`"), ("\\", "`\\\\`"), ("1\n\tABC", "`1\\n\\tABC`")],
     )
-    def test_encode_single_java_with_dollars(self, value, expected):
+    def test_encode_single_with_dollars(self, value, expected):
         literal_string = LiteralString()
         actual = literal_string._encodeSinglePython(value)
         assert actual == expected.replace("`", '"')
@@ -30,10 +30,13 @@ class TestLiteralString:
         [
             ("1", "'''\n1'''"),
             ("\\", "'''\n\\\\'''"),
-            ("  leading\ntrailing  ", "'''\n" + "\\s leading\n" + "trailing \\s'''"),
+            (
+                "  leading\ntrailing  ",
+                "'''\n" + "\x20 leading\n" + "trailing \x20'''",
+            ),
         ],
     )
-    def test_encode_multi_java(self, value, expected):
+    def test_encode_multi(self, value, expected):
         literal_string = LiteralString()
         actual = literal_string.encodeMultiPython(value, EscapeLeadingWhitespace.NEVER)
         assert actual == expected.replace("'", '"')
@@ -41,7 +44,7 @@ class TestLiteralString:
     @pytest.mark.parametrize(
         "value, expected", [("1", "1"), ("\\\\", "\\"), ("1\\n\\tABC", "1\n\tABC")]
     )
-    def test_parse_single_java(self, value, expected):
+    def test_parse_single(self, value, expected):
         literal_string = LiteralString()
         actual = literal_string._parseSinglePython(f'"{value.replace("'", "\"")}"')
         assert actual == expected
@@ -56,7 +59,7 @@ class TestLiteralString:
             ("\n  123  \\s\n  abc\t\\s", "123   \nabc\t "),
         ],
     )
-    def test_parse_multi_java(self, value, expected):
+    def test_parse_multi(self, value, expected):
         literal_string = LiteralString()
         actual = literal_string.parseMultiPython(f'"""{value.replace("'", "\"")}"""')
         assert actual == expected
