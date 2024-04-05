cc0810dee930fa08c0470a6d350d400ab34e8651
Ned Twigg <ned.twigg@diffplug.com>
2024-04-04 23:30:10 -0700

    Stub out DiskSelfie and StringSelfie

---
+++ python/selfie-lib/selfie_lib/SelfieImplementations.py
@@ -0,0 +1,31 @@
+from socketserver import ThreadingUnixStreamServer
+from Snapshot import Snapshot
+from SnapshotSystem import DiskStorage
+
+
+class FluentFacet:
+    pass
+
+
+class DiskSelfie(FluentFacet):
+    def __init__(self, actual: Snapshot, disk: DiskStorage):
+        self._actual = actual
+        self._disk = disk
+
+    def toMatchDisk(self, sub="") -> "DiskSelfie":
+        raise NotImplementedError()
+
+    def toMatchDisk_TODO(self, sub="") -> "DiskSelfie":
+        raise NotImplementedError()
+
+
+class StringSelfie(DiskSelfie):
+    def __init__(self, actual: Snapshot, disk: DiskStorage, expected: str):
+        super().__init__(actual, disk)
+        self._expected = expected
+
+    def toBe_TODO(self) -> str:
+        raise NotImplementedError()
+
+    def toBe(self, expected: str) -> str:
+        raise NotImplementedError()
