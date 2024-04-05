248929a5b15123d35b6a57e1d8f18671913b9986
Harvir Sahota <hssahota2@usfca.edu>
2024-04-04 20:58:43 -0700

    Initial expectSelfie implementation

--- python/example-pytest-selfie/tests/Simple_test.py
+++ python/example-pytest-selfie/tests/Simple_test.py
@@ -1,6 +1,10 @@
 from selfie_lib.ArrayMap import ArrayMap
+from pytest_selfie.expectSelfie import expectSelfie
 
 
 def test_simple():
     test = ArrayMap.empty().plus("key", "value")
     assert test.__len__() == 1
+
+def test_comment_removal(): #selfieonce
+    expectSelfie("nothing happens")
