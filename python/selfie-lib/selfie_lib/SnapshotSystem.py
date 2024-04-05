13083e218d1d1177700669d055edfd47c2cb1046
Ned Twigg <ned.twigg@diffplug.com>
2024-04-04 23:29:10 -0700

    Stub out SnapshotSystem, especially its global

--- python/selfie-lib/selfie_lib/SnapshotSystem.py
+++ python/selfie-lib/selfie_lib/SnapshotSystem.py
@@ -24,3 +24,36 @@ class FS(ABC):
     @abstractmethod
     def assert_failed(self, message: str, expected=None, actual=None) -> Exception:
         pass
+
+
+# TODO: port DiskStorage into Python, it will be all @abstractmethod just like FS
+class DiskStorage:
+    def __init__(self):
+        pass
+
+
+# TODO: port SnapshotSystem into Python, it will be all @abstractmethod just like FS
+class SnapshotSystem:
+    def __init__(self):
+        pass
+
+    def diskThreadLocal() -> "DiskStorage":
+        raise NotImplementedError()
+
+
+selfieSystem = None
+
+
+def _initSelfieSystem(system: SnapshotSystem):
+    global selfieSystem
+    if selfieSystem is not None:
+        raise Exception("Selfie system already initialized")
+    selfieSystem = system
+
+
+def _selfieSystem() -> "SnapshotSystem":
+    if selfieSystem is None:
+        raise Exception(
+            "Selfie system not initialized, make sure that `pytest-selfie` is installed and that you are running tests with `pytest`."
+        )
+    return selfieSystem
