7897c6e6e4e81ea66b7934e40bfde5453ec58ffd
Ned Twigg <ned.twigg@diffplug.com>
2024-04-17 01:52:26 -0700

    Update format.

--- python/example-pytest-selfie/tests/Simple_test.py
+++ python/example-pytest-selfie/tests/Simple_test.py
@@ -8,7 +8,7 @@ from selfie_lib.Selfie import expectSelfie
 
 
 def test_comment_removal():  # selfieonce
-    expectSelfie("nothing happens").toBe_TODO() 
+    expectSelfie("nothing happens").toBe_TODO()
 
 
 def test_inline():
@@ -20,5 +20,6 @@ def test_inline():
 def test_disk():
     expectSelfie("A").toMatchDisk_TODO()
 
-    expectSelfie("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,").toMatchDisk_TODO()
-    
+    expectSelfie(
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,"
+    ).toMatchDisk_TODO()
