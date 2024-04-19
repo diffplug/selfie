d9fb203ec43484fe8bfdad0a5fadedfbe8fcb694
Ned Twigg <ned.twigg@diffplug.com>
2024-04-18 18:31:06 -0700

    More fixup.

--- python/example-pytest-selfie/tests/Simple_test.py
+++ python/example-pytest-selfie/tests/Simple_test.py
@@ -12,14 +12,15 @@ def test_comment_removal():  # selfieonce
 
 
 def test_inline():
+    expect_selfie(1).to_be_TODO(1)
     expect_selfie("A").to_be_TODO()
 
     expect_selfie("testing123").to_be_TODO()
 
 
 def test_disk():
-    expect_selfie("A").toMatchDisk_TODO()
+    expect_selfie("A").to_match_disk_TODO()
 
     expect_selfie(
         "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,"
-    ).toMatchDisk_TODO()
+    ).to_match_disk_TODO()
--- python/selfie-lib/selfie_lib/Selfie.py
+++ python/selfie-lib/selfie_lib/Selfie.py
@@ -1,9 +1,17 @@
+from tkinter import NO
 from .SelfieImplementations import StringSelfie
 from .Snapshot import Snapshot
 from .SnapshotSystem import _selfieSystem
 
+from typing import Union
 
-def expectSelfie(actual: str) -> "StringSelfie":
-    snapshot = Snapshot.of(actual)
-    diskStorage = _selfieSystem().disk_thread_local()
-    return StringSelfie(snapshot, diskStorage)
+
+def expect_selfie(actual: Union[str, int]) -> "StringSelfie":
+    if actual is int:
+        raise NotImplementedError()
+    elif actual is str:
+        snapshot = Snapshot.of(actual)
+        diskStorage = _selfieSystem().disk_thread_local()
+        return StringSelfie(snapshot, diskStorage)
+    else:
+        raise NotImplementedError()
