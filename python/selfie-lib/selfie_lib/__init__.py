8e26d4b8707f1444cd5bddbe802cec5a1def2785
Ned Twigg <ned.twigg@diffplug.com>
2024-04-19 17:12:14 -0700

    Add missing exports from `selfie-lib`.

--- python/selfie-lib/selfie_lib/__init__.py
+++ python/selfie-lib/selfie_lib/__init__.py
@@ -1,5 +1,7 @@
 # maintain alphabetical order
 from .ArrayMap import ArrayMap as ArrayMap
+from .ArrayMap import ArraySet as ArraySet
+from .Atomic import AtomicReference as AtomicReference
 from .CommentTracker import CommentTracker as CommentTracker
 from .FS import FS as FS
 from .LineReader import LineReader as LineReader
@@ -11,6 +13,7 @@ from .Snapshot import Snapshot as Snapshot
 from .SnapshotFile import SnapshotFile as SnapshotFile
 from .SnapshotReader import SnapshotReader as SnapshotReader
 from .SnapshotSystem import _initSelfieSystem as _initSelfieSystem
+from .SnapshotSystem import _clearSelfieSystem as _clearSelfieSystem
 from .SnapshotSystem import _selfieSystem as _selfieSystem
 from .SnapshotSystem import DiskStorage as DiskStorage
 from .SnapshotSystem import Mode as Mode
@@ -19,7 +22,10 @@ from .SnapshotValue import SnapshotValue as SnapshotValue
 from .SnapshotValueReader import SnapshotValueReader as SnapshotValueReader
 from .SourceFile import SourceFile as SourceFile
 from .TypedPath import TypedPath as TypedPath
+from .WithinTestGC import WithinTestGC as WithinTestGC
 from .WriteTracker import CallLocation as CallLocation
 from .WriteTracker import CallStack as CallStack
+from .WriteTracker import DiskWriteTracker as DiskWriteTracker
+from .WriteTracker import InlineWriteTracker as InlineWriteTracker
 from .WriteTracker import recordCall as recordCall
 from .WriteTracker import SnapshotFileLayout as SnapshotFileLayout
