9c4515a8bf0baf4e67bcfb37bfb1768a151d62f1
Ned Twigg <ned.twigg@diffplug.com>
2024-04-22 15:22:47 -0700

    Fixup the test structure a bit.

--- python/example-pytest-selfie/tests/simple_comment_removal_test.py
+++ python/example-pytest-selfie/tests/simple_comment_removal_test.py
@@ -2,4 +2,4 @@ from selfie_lib.Selfie import expect_selfie
 
 
 def test_comment_removal():  # selfieonce
-    expect_selfie("nothing happens").to_be_TODO()
+    expect_selfie("no op").to_be("no op")
