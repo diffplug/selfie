b5d4d6e3bdb815e49ab614f19a6e0c561ecd08bf
Ned Twigg <ned.twigg@diffplug.com>
2024-04-22 14:11:30 -0700

    Fix `expect_selfie` assertions.

--- python/selfie-lib/selfie_lib/Selfie.py
+++ python/selfie-lib/selfie_lib/Selfie.py
@@ -1,4 +1,3 @@
-from tkinter import NO
 from .SelfieImplementations import StringSelfie
 from .Snapshot import Snapshot
 from .SnapshotSystem import _selfieSystem
@@ -7,9 +6,9 @@ from typing import Union
 
 
 def expect_selfie(actual: Union[str, int]) -> "StringSelfie":
-    if actual is int:
+    if isinstance(actual, int):
         raise NotImplementedError()
-    elif actual is str:
+    elif isinstance(actual, str):
         snapshot = Snapshot.of(actual)
         diskStorage = _selfieSystem().disk_thread_local()
         return StringSelfie(snapshot, diskStorage)
