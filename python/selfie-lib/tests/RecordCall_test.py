7f36677802e72c7946884232fd5a5935b00d8c6b
Ned Twigg <ned.twigg@diffplug.com>
2024-05-04 23:24:31 -0700

    Fix test broken by line number.

--- python/selfie-lib/tests/RecordCall_test.py
+++ python/selfie-lib/tests/RecordCall_test.py
@@ -29,7 +29,7 @@ def test_record_call_with_caller_file_only_false():
         len(call_stack.rest_of_stack) > 0
     ), "Expected the rest of stack to contain more than one CallLocation"
 
-    expected_call_location_str = "File: RecordCall_test.py, Line: 25"
+    expected_call_location_str = "File: RecordCall_test.py, Line: 26"
 
     if call_stack.location.file_name is not None:
         actual_file_name = os.path.basename(call_stack.location.file_name)
