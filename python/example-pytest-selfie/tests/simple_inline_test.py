eb581521d34771fd578ba5a287ddacfe9c7e823e
Ned Twigg <ned.twigg@diffplug.com>
2024-05-04 22:43:42 -0700

    Fix ruff.

--- python/example-pytest-selfie/tests/simple_inline_test.py
+++ python/example-pytest-selfie/tests/simple_inline_test.py
@@ -13,5 +13,5 @@ def test_write():
     expect_selfie("B").to_be("B")
     expect_selfie(20000).to_be(20_000)
     expect_selfie([1, 2, 3]).to_be([1, 2, 3])
-    expect_selfie(("a", 2, 3)).to_be(('a', 2, 3))
-    expect_selfie({"a": 1, "b": 2}).to_be({'a': 1, 'b': 2})
+    expect_selfie(("a", 2, 3)).to_be(("a", 2, 3))
+    expect_selfie({"a": 1, "b": 2}).to_be({"a": 1, "b": 2})
