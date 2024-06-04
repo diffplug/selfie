d7b3977151cab7e691365c525e1fe75d3eecf40b
Ned Twigg <ned.twigg@diffplug.com>
2024-06-03 23:33:31 -0700

    Fully unify `PerCharacterEscaper`.

--- jvm/selfie-lib/src/commonMain/kotlin/com/diffplug/selfie/PerCharacterEscaper.kt
+++ jvm/selfie-lib/src/commonMain/kotlin/com/diffplug/selfie/PerCharacterEscaper.kt
@@ -23,31 +23,141 @@ internal expect fun codePoints(s: String): IntArray
 
 internal expect fun StringBuilder.appendCP(codePoint: Int): StringBuilder
 
-expect class PerCharacterEscaper {
-  fun escape(input: String): String
-  fun unescape(input: String): String
+/**
+ * If your escape policy is "'123", it means this:
+ * ```
+ * abc->abc
+ * 123->'1'2'3
+ * I won't->I won''t
+ * ```
+ */
+class PerCharacterEscaper
+/**
+ * The first character in the string will be uses as the escape character, and all characters will
+ * be escaped.
+ */
+private constructor(
+    private val escapeCodePoint: Int,
+    private val escapedCodePoints: IntArray,
+    private val escapedByCodePoints: IntArray
+) {
+  private fun firstOffsetNeedingEscape(input: String): Int {
+    val length = input.length
+    var firstOffsetNeedingEscape = -1
+    var offset = 0
+    outer@ while (offset < length) {
+      val codepoint = codePointAt(input, offset)
+      for (escaped in escapedCodePoints) {
+        if (codepoint == escaped) {
+          firstOffsetNeedingEscape = offset
+          break@outer
+        }
+      }
+      offset += charCount(codepoint)
+    }
+    return firstOffsetNeedingEscape
+  }
+  fun escape(input: String): String {
+    val noEscapes = firstOffsetNeedingEscape(input)
+    return if (noEscapes == -1) {
+      input
+    } else {
+      val length = input.length
+      val needsEscapes = length - noEscapes
+      val builder = StringBuilder(noEscapes + 4 + needsEscapes * 5 / 4)
+      builder.append(input, 0, noEscapes)
+      var offset = noEscapes
+      while (offset < length) {
+        val codepoint = codePointAt(input, offset)
+        offset += charCount(codepoint)
+        val idx = indexOf(escapedCodePoints, codepoint)
+        if (idx == -1) {
+          builder.appendCP(codepoint)
+        } else {
+          builder.appendCP(escapeCodePoint)
+          builder.appendCP(escapedByCodePoints[idx])
+        }
+      }
+      builder.toString()
+    }
+  }
+  private fun firstOffsetNeedingUnescape(input: String): Int {
+    val length = input.length
+    var firstOffsetNeedingEscape = -1
+    var offset = 0
+    while (offset < length) {
+      val codepoint = codePointAt(input, offset)
+      if (codepoint == escapeCodePoint) {
+        firstOffsetNeedingEscape = offset
+        break
+      }
+      offset += charCount(codepoint)
+    }
+    return firstOffsetNeedingEscape
+  }
+  fun unescape(input: String): String {
+    val noEscapes = firstOffsetNeedingUnescape(input)
+    return if (noEscapes == -1) {
+      input
+    } else {
+      val length = input.length
+      val needsEscapes = length - noEscapes
+      val builder = StringBuilder(noEscapes + 4 + needsEscapes * 5 / 4)
+      builder.append(input, 0, noEscapes)
+      var offset = noEscapes
+      while (offset < length) {
+        var codepoint = codePointAt(input, offset)
+        offset += charCount(codepoint)
+        // if we need to escape something, escape it
+        if (codepoint == escapeCodePoint) {
+          if (offset < length) {
+            codepoint = codePointAt(input, offset)
+            val idx = indexOf(escapedByCodePoints, codepoint)
+            if (idx != -1) {
+              codepoint = escapedCodePoints[idx]
+            }
+            offset += charCount(codepoint)
+          } else {
+            throw IllegalArgumentException(
+                StringBuilder()
+                    .append("Escape character '")
+                    .appendCP(escapeCodePoint)
+                    .append("' can't be the last character in a string.")
+                    .toString())
+          }
+        }
+        // we didn't escape it, append it raw
+        builder.appendCP(codepoint)
+      }
+      builder.toString()
+    }
+  }
 
   companion object {
-    /**
-     * If your escape policy is `'123`, it means this: <br>
-     *
-     * ```
-     * abc->abc
-     * 123->'1'2'3
-     * I won't->I won''t
-     * ```
-     */
-    fun selfEscape(escapePolicy: String): PerCharacterEscaper
-
-    /**
-     * If your escape policy is `'a1b2c3d`, it means this: <br>
-     *
-     * ```
-     * abc->abc
-     * 123->'b'c'd
-     * I won't->I won'at
-     * ```
-     */
-    fun specifiedEscape(escapePolicy: String): PerCharacterEscaper
+    private fun indexOf(arr: IntArray, target: Int): Int {
+      for ((index, value) in arr.withIndex()) {
+        if (value == target) {
+          return index
+        }
+      }
+      return -1
+    }
+    fun selfEscape(escapePolicy: String): PerCharacterEscaper {
+      val escapedCodePoints = codePoints(escapePolicy)
+      val escapeCodePoint = escapedCodePoints[0]
+      return PerCharacterEscaper(escapeCodePoint, escapedCodePoints, escapedCodePoints)
+    }
+    fun specifiedEscape(escapePolicy: String): PerCharacterEscaper {
+      val codePoints = codePoints(escapePolicy)
+      require(codePoints.size % 2 == 0)
+      val escapeCodePoint = codePoints[0]
+      val escapedCodePoints = IntArray(codePoints.size / 2)
+      val escapedByCodePoints = IntArray(codePoints.size / 2)
+      for (i in escapedCodePoints.indices) {
+        escapedCodePoints[i] = codePoints[2 * i]
+        escapedByCodePoints[i] = codePoints[2 * i + 1]
+      }
+      return PerCharacterEscaper(escapeCodePoint, escapedCodePoints, escapedByCodePoints)
+    }
   }
 }
