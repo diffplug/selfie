7e0198d620e82afec7e984dc42c1656f62df0103
Ned Twigg <ned.twigg@diffplug.com>
2025-02-06 13:15:17 -0800

    Selfie JUnit4 compiles.

---
+++ jvm/selfie-runner-junit4/src/main/kotlin/com/diffplug/selfie/junit4/SnapshotFileLayoutJUnit4.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2024-2025 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.selfie.junit4
+
+class SnapshotFileLayoutJUnit4(className: String) {
+  fun incrementContainers() {
+    TODO("Coroutine support not implemented for JUnit4")
+  }
+  fun startTest(testName: String, isContainer: Boolean) {
+    TODO("Coroutine support not implemented for JUnit4")
+  }
+  fun finishedTestWithSuccess(testName: String, isContainer: Boolean, wasSuccessful: Boolean) {
+    TODO("Coroutine support not implemented for JUnit4")
+  }
+  fun decrementContainersWithSuccess(wasSuccessful: Boolean) {
+    TODO("Coroutine support not implemented for JUnit4")
+  }
+}
