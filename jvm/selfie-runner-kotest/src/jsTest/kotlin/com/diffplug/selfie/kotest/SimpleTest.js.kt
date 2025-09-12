cc0d0a2c370d5a21a459cac6459d71c737ff5c11
Ned Twigg <ned.twigg@diffplug.com>
2025-09-12 09:35:33 -0700

    Add a test to fix the JS emptiness.

---
+++ jvm/selfie-runner-kotest/src/jsTest/kotlin/com/diffplug/selfie/kotest/SimpleTest.js.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2025 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.selfie.kotest
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ * The only purpose of this test is to make sure that there is at least one test working for the JS
+ * test runner. If there are any real tests running at all, then this should be deleted.
+ */
+class ToMakeJsWorkDeleteThisTest {
+  @Test
+  fun simpleTest() {
+    assertEquals(2, 1 + 1)
+  }
+}
